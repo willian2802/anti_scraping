f386942bcd92ba371ab731d602aa8696f1784824
Willian Souza Nascimento <willian_2801@hotmail.com>
2024-06-25 15:44:21 +0100

    clean the code and pick time of request

--- python_server/app.py
+++ python_server/app.py
@@ -1,7 +1,6 @@
-from datetime import datetime
 from flask import Flask, request, abort
 from views import views
-
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -19,25 +18,16 @@ access_log = {}  #dicionário para registrar as requisições
 @app.route('/')
 def block_user_for():
 
-    # Define the list of blocked IP addresses
     ip_address = request.remote_addr
-
-    if ip_address not in access_log:
-        access_log[ip_address] = []
-    access_log[ip_address].append(request.path)
-
-    #  se passar o limite de request o ip vai ser bloqueado
-    if len(access_log[ip_address]) > 10:  # Ajuste o limite conforme necessário
-        return abort(403)  # Bloqueia IPs com muitas requisições
 
     # bloqueia os agents que tenhan o nome "bot" ou "scraper"
     user_agent = request.headers.get('User-Agent')
     if "bot" in user_agent.lower() or "scraper" in user_agent.lower():
-        return abort(403)
+        return "Access denied. Bots or scrapers are not pertmitted" 
     
     # se o agente for "headless" nao vai ter acesso ao site    
     if "Headless" in user_agent:
-        return abort(403)
+        return "Access denied. headless are not pertmitted" 
     
     if ip_address not in access_log:
         access_log[ip_address] = []
@@ -52,15 +42,29 @@ def block_user_for():
     if ip_address in black_list_IP:
         return f"Access denied. UserIP:{ip_address}, You are blocked from accessing this page."
     
-    if ip_address not in access_log:
-        access_log[ip_address] = []
-    access_log[ip_address].append(request.path)
-    
-    #  se passar o limite de request o ip vai ser bloqueado
-    if len(access_log[ip_address]) > 100:  # Ajuste o limite conforme necessário
-        return abort(403)  # Bloqueia IPs com muitas requisições
+# -------------------------------- update_access_log and list_IPs--------------------------------
+# @app.route('/IPs_list')
+# def list_IPs_updated():
+
+#     ip_address = request.remote_addr
+#     if ip_address not in access_log:
+#         access_log[ip_address] = []
+#     # tempo_para_limpar = datetime.now() - timedelta(hours=6)
+
+#     # for ip_user in access_log: 
+#     #     if ip_user.current_time > tempo_para_limpar:
+#     #         access_log[ip_address].remove("/endereco-de-processamento")
+#     #         access_log[ip_address].remove("12:14:14")
+
+#     # access_log[ip_address].append("/endereco-de-processamento")
+#     # access_log[ip_address].append("12:14:14")
+#     # access_log[ip_address].append("11:14:14")
 
 
+#     return f"Access Log: {access_log}"
+
+
+# -------------------------------- trap_activated = honneypot --------------------------------
 @app.route('/endereco-de-processamento', methods=['POST'])
 def trap_activated():
     ip_address = request.remote_addr
@@ -71,7 +75,16 @@ def trap_activated():
 
     #adiciona o ip na yellow list
     yellow_list_IP.append(request.remote_addr)
-    return f"Time: {current_time}, User IP:{ip_address}"
+
+
+    if ip_address not in access_log:
+        access_log[ip_address] = []
+    
+    # adiciona no access_log o path da requisição e o tempo da requisição
+    access_log[ip_address].append(request.path)
+    access_log[ip_address].append(current_time)
+
+    return f"{access_log}"
 
 if __name__ == '__main__':
     app.run(debug=True)
