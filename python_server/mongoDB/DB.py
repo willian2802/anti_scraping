76b2c7f4ca99fb054b5a51f6e38ab6ba4309f39d
Willian Souza Nascimento <willian_2801@hotmail.com>
2024-07-29 16:34:04 +0100

    MongoDB conection

---
+++ python_server/mongoDB/DB.py
@@ -0,0 +1,43 @@
+import os
+import certifi
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Obter a URI do MongoDB do arquivo .env
+MONGODB_URI = os.getenv('MONGODB_URI')
+
+# Caminho para o certificado SSL
+ca = certifi.where()
+
+# Conectar ao MongoDB usando a URI e o certificado SSL
+client = MongoClient(MONGODB_URI, tlsCAFile=ca)
+
+# Listar nomes dos bancos de dados para verificar a conexão
+try:
+    for db in client.list_database_names():
+        print(db)
+except Exception as e:
+    print(f"Erro ao listar bancos de dados: {e}")
+
+
+# ------------------------------ test ------------------------------
+
+
+# from pymongo.mongo_client import MongoClient
+# from pymongo.server_api import ServerApi
+
+# uri = "mongodb+srv://williansouza11922:<password>@cluster1.yenq4ds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
+
+# # Create a new client and connect to the server
+# client = MongoClient(uri, server_api=ServerApi('1'))
+
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
+
---
+++ python_server/mongoDB/conection.py
@@ -0,0 +1,1 @@
+MONGODB_URI = "mongodb+srv://williansouza11922:<Njx5AEy1VsRIJvsu>@cluster1.yenq4ds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
